81a46b11dd5c448c7cb0b35b2b5c36157449b5ed
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-02-26 22:31:49 +0300

    add some icons to auth screen

--- lib/features/auth/presentation/screens/auth_screen.dart
+++ lib/features/auth/presentation/screens/auth_screen.dart
@@ -1,28 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:pusher/features/auth/presentation/controller/auth_controller.dart';
 
 class AuthScreen extends GetView<AuthController> {
   const AuthScreen({super.key});
+  Duration get loginTime => const Duration(milliseconds: 2250);
 
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
-      title: 'Chat',
+      title: 'Chat Pusher',
       logo: const AssetImage('assets/image/chat.png'),
-      onLogin: (loginData) {
+      onLogin: (p0) {
         return;
       },
-      onSignup: (signupData) {
+      onSignup: (p0) {
         return;
       },
+
+      loginProviders: <LoginProvider>[
+        LoginProvider(
+          icon: FontAwesomeIcons.google,
+          label: 'Google',
+          callback: () async {
+            debugPrint('start google sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop google sign in');
+            return null;
+          },
+        ),
+        LoginProvider(
+          icon: FontAwesomeIcons.facebookF,
+          label: 'Facebook',
+          callback: () async {
+            debugPrint('start facebook sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop facebook sign in');
+            return null;
+          },
+        ),
+        LoginProvider(
+          icon: FontAwesomeIcons.linkedinIn,
+          label: 'Linked',
+          callback: () async {
+            debugPrint('start linkdin sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop linkdin sign in');
+            return null;
+          },
+        ),
+        LoginProvider(
+          icon: FontAwesomeIcons.githubAlt,
+          label: 'Github',
+          callback: () async {
+            debugPrint('start github sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop github sign in');
+            return null;
+          },
+        ),
+      ],
       onSubmitAnimationCompleted: () {
         // Navigator.of(context).pushReplacement(MaterialPageRoute(
         //   builder: (context) => const DashboardScreen(),
         // ));
       },
-      onRecoverPassword: (val) {
+      onRecoverPassword: (p0) {
         return;
       },
     );
